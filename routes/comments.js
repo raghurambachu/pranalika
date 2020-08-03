84b9937996a52d3ff536762bc5ad5102eb501aa3
Raghuram <1993raghuram@gmail.com>
2020-08-03 15:32:23 +0530

    View Comments for particular user

--- routes/comments.js
+++ routes/comments.js
@@ -6,6 +6,16 @@ const User = require("../models/User");
 
 const router = express.Router();
 
+router.get("/list", auth.verifyUserLoggedIn, (req, res, next) => {
+  const userId = req.user._id;
+  Comment.find({ userId })
+    .populate("articleId", "-description -updatedAt")
+    .exec((err, comments) => {
+      if (err) return next(err);
+      res.render("comments-list", { comments });
+    });
+});
+
 router.get("/:id/delete", auth.verifyUserLoggedIn, (req, res, next) => {
   const commentId = req.params.id;
   Comment.findOne({ _id: commentId })
@@ -31,8 +41,29 @@ router.get("/:id/delete", auth.verifyUserLoggedIn, (req, res, next) => {
             }
           );
         });
+      } else {
+        res.redirect(`/articles/${comment.articleId}`);
       }
     });
+});
+
+router.post("/:id/update", auth.verifyUserLoggedIn, (req, res, next) => {
+  const commentId = req.params.id;
+  Comment.findById(commentId, (err, comment) => {
+    if (err) return next(err);
+    if (req.user._id.toString() === comment.userId.toString()) {
+      Comment.findByIdAndUpdate(
+        commentId,
+        { content: req.body.content },
+        (err, updatedComment) => {
+          if (err) return next(err);
+          res.redirect(`/articles/${comment.articleId}`);
+        }
+      );
+    } else {
+      res.redirect(`/articles/${comment.articleId}`);
+    }
+  });
 });
 
 module.exports = router;
